040317c8f8751ca9d1187f62af0f44ab9eba2699
Thomas Lee <djvoa12@gmail.com>
2019-09-18 23:53:43 -0400

    added redux action-creators

---
+++ src/actions/index.js
@@ -0,0 +1,36 @@
+import _ from 'lodash';
+import jsonplaceholder from '../apis/jsonplaceholder';
+
+export const fetchPostsAndUser = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  const posts = getState().posts;
+  const userIds = _.uniq(_.map(posts, 'userId'));
+  userIds.forEach((id) => {
+    dispatch(fetchUser(id))
+  });
+
+  // _.chain(posts)
+  //   .map('userIds')
+  //   .uniq()
+  //   .forEach((id) => dispatch(fetchUser(id))
+  //   .value();
+};
+
+export const fetchPosts = () => {
+  return async (dispatch /*, getState */) => {
+    const response = await jsonplaceholder.get('/posts');
+    dispatch({ type: 'FETCH_POSTS', payload: response.data });
+  }
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonplaceholder.get(`/users/${id}`);
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+// Memoized Version:
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonplaceholder.get(`/users/${id}`);
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
